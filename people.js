832d5994c34a90a434e746e34f0113cb1a62f24b
Emanuele Dinaro <ema.dinaro@gmail.com>
2025-01-16 17:19:44 +0100

    feat: add function person

--- people.js
+++ people.js
@@ -3,21 +3,26 @@
 // 1. Importa la tua funzione da names.js
 // 2. Importa la tua funzione da hobbies.js
 // 3. Crea una funzione che non ha parametri. 
-// La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
+// *La funzione dovrebbe restituire un oggetto con due proprietà: fullName ed hobbies. 
 // All'interno della tua funzione, usa le tue due funzioni precedenti per costruire l'oggetto.
 
 // 1. Importa la tua funzione da names.js
 const name = require("./names.js")
+
 // 2. Importa la tua funzione da hobbies.js
 const hobby = require("./hobbies.js")
 
-const fullName = name("Emanuele", "Dinaro");
-const hobbies = hobby("Visite mediche", "Spendere soldi", "League of Legends")
-
-const infoPerson = {
-    fullName,
-    hobbies
+// 3. Crea una funzione che non ha parametri*.
+function person() {
+    const fullName = name("Emanuele", "Dinaro");
+    const hobbies = hobby("Visite mediche", "Spendere soldi", "League of Legends")
+    
+    const infoPerson = {
+        fullName,
+        hobbies
+    }
+    return infoPerson
 }
 
-console.log(infoPerson);
+console.log(person());
 
